3a37cd9f7784e45fddfd03d3b5f2ba734f241297
t2024-m0164 <t2024-m0164@t2024-m0164ui-MacBookAir.local>
2024-11-14 10:19:21 +0900

    2

--- src/Problem/Solution1.java
+++ src/Problem/Solution1.java
@@ -1,7 +1,7 @@
-package Problem1;
+package Problem;
 //정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
 
-class Solution {
+class Solution1 {
     public int solution(int num1, int num2) {
         int answer = 0;
 
