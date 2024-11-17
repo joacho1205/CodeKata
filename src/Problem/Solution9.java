5e140f94f094ca55f9dad9619d5f90f244874f25
joacho <joacho1205@gmail.com>
2024-11-17 17:38:38 +0900

    1117

---
+++ src/Problem/Solution9.java
@@ -0,0 +1,13 @@
+package Problem;
+
+class Solution9 {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 2; i <= n ; i+=2) {
+            answer += i;
+        }
+
+        return answer;
+    }
+}
