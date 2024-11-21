2a2fc35496862591dca81ee7a1b4a1ba522e6074
joacho <joacho1205@gmail.com>
2024-11-21 11:04:34 +0900

    1121

--- src/Problem/Solution13.java
+++ src/Problem/Solution13.java
@@ -12,3 +12,21 @@ public class Solution13 {
         return answer;
     }
 }
+
+/*
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String num = String.valueOf(n);
+
+        for(int i = 0 ; i < num.length() ; i++) {
+            answer += Integer.parseInt(String.valueOf(num.charAt(i)));
+        }
+
+        return answer;
+    }
+}
+ */
