532fdcbbace5d7e96297c6da89b9d5a438b0e298
joacho <joacho1205@gmail.com>
2024-11-25 16:55:33 +0900

    1125

---
+++ src/Problem/Solution17.java
@@ -0,0 +1,13 @@
+package Problem;
+
+class Solution17 {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        int[] answer = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            answer[i] = Integer.parseInt(s.substring(s.length() - 1 - i, s.length() - i));
+        }
+
+        return answer;
+    }
+}
