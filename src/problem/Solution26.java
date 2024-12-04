df0ad99a058148bd27bb545cbc6dfc7566c598f2
joacho <joacho1205@gmail.com>
2024-12-04 09:56:57 +0900

    1204

---
+++ src/problem/Solution26.java
@@ -0,0 +1,11 @@
+package problem;
+
+class Solution26 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
+        }
+        return answer;
+    }
+}
