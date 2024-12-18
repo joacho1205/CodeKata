37ddaf95e82d5ea2a97d9884fb659c1587961ebe
joacho <joacho1205@gmail.com>
2024-12-18 16:23:40 +0900

    1218

---
+++ src/problem/Solution40.java
@@ -0,0 +1,12 @@
+package problem;
+
+class Solution40 {
+    public int solution(int n) {
+        String answer = "";
+        while(n > 0) {
+            answer += n % 3;
+            n /= 3;
+        }
+        return Integer.parseInt(answer,3);
+    }
+}
