3d848dea839733ac85941f5a505225288aa96462
joacho <joacho1205@gmail.com>
2024-12-22 22:55:51 +0900

    1222

---
+++ src/problem/Solution44.java
@@ -0,0 +1,20 @@
+package problem;
+
+class Solution44 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max_row = 0;
+        int max_col = 0;
+
+        for(int i = 0; i < sizes.length; i++) {
+            int row = Math.max(sizes[i][0],sizes[i][1]);
+            int col = Math.min(sizes[i][0],sizes[i][1]);
+
+            max_row = Math.max(max_row,row);
+            max_col = Math.max(max_col,col);
+        }
+        answer = max_row * max_col;
+
+        return answer;
+    }
+}
