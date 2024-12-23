2e10719f88241da9c49ac16d54201b0f8b67a2bc
joacho <joacho1205@gmail.com>
2024-12-23 23:52:55 +0900

    1223

---
+++ src/problem/Solution45.java
@@ -0,0 +1,24 @@
+package problem;
+
+class Solution45 {
+    public String solution(String s, int n) {
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == ' ') {
+                answer += ch;
+                continue;
+            }
+
+            if (ch >= 'a' && ch <= 'z') {
+                answer += (char) ((ch - 'a' + n) % 26 + 'a');
+            } else {
+                answer += (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+
+        }
+        return answer;
+    }
+}
